09ce46b2e4a6b5073ec3d6119fe619159ea6c31d
Julio Quintana <julioeq29@gmail.com>
2019-02-19 18:39:44 +0000

    exercise working more or less

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  # As a user, I can list tasks
+  # As a user, I can view the details of a task
+
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # As a user, I can add a new task
+  post 'tasks', to: 'tasks#create'
+  # As a user, I can edit a task (mark as completed / update title & details)
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # As a user, I can remove a task
+  delete 'tasks/:id', to: 'tasks#update', as: :delete
+
 end
